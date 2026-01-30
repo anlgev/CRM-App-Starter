0d8d9daea6282eb8946cf425b0613b9213236f67
anlgev <github-mailin@...>
2026-01-31 02:24:30 +0300

    Fix Dockerfile name, move models into app directory and add __init__.py in app directory

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,5 @@
 from app.database import Base
 from app.models.company import Company
-from app.models.deal import Deal
+from app.models.deals import Deal
 from app.models.activity import Activity
 from app.models.user import User
